34065f811b770e52c44dec9dd54779cc5e520b33
mbudellini <matteo.budellini@gmail.com>
2026-04-22 16:31:17 +0200

    login and register functioning

--- what_frontend/src/pages/Login.jsx
+++ what_frontend/src/pages/Login.jsx
@@ -1,22 +1,46 @@
 import { useState } from "react";
-
+import axios from "axios";
+import { useNavigate } from "react-router";
 
 function Login() {
-    const [email, setEmail] = useState('')
-const [password, setPassword] = useState('')
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const handleEmail = (e) => {
     setEmail(e.target.value);
   };
   const handlePassword = (e) => {
     setPassword(e.target.value);
   };
+  const navigate = useNavigate();
+  const buttonClick = async () => {
+    if (!email || !password || !confirm) {
+      console.log("Some data is missing");
+    }
+    try {
+      let result = await axios.post("http://localhost:4444/users/login", {
+        email,
+        password,
+      });
+      if (result.data.ok) {
+        navigate("/");
+      }
+      console.log(result);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
-    <div className="sideMargin2">
-      <p>email</p>
-      <input type="email" onChange={handleEmail}></input>
-      <p>password</p>
-      <input type="password" onChange={handlePassword}></input>
+    <div className="center">
+      <div>
+        <p>email</p>
+        <input type="email" onChange={handleEmail}></input>
+        <p>password</p>
+        <input type="password" onChange={handlePassword}></input>
+        <p></p>
+         <button onClick={buttonClick}>Login</button>
+      </div>
+     
     </div>
   );
 }
--- what_frontend/src/pages/Register.jsx
+++ what_frontend/src/pages/Register.jsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import axios from 'axios'
+import {useNavigate} from 'react-router'
 
 function Register() {
   const [email, setEmail] = useState();
@@ -13,14 +15,42 @@ function Register() {
   const handleConfirm = (e) => {
     setConfirm(e.target.value);
   };
+  const navigate =useNavigate()
+  const buttonClick = async () => {
+    if (!email || !password || !confirm) {
+      console.log("Some data is missing");
+    }
+    if (password !== confirm) {
+      console.log("passwords dont match");
+    }
+    try {
+      let result = await axios.post("http://localhost:4444/users/register", {
+        email,
+        password,
+        password2: confirm,
+      })
+      if(result.data.ok){
+        navigate('/users/login')
+
+      }
+      console.log(result);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <div className="sideMargin2">
+    <div className="center">
+        <div>
       <p>email</p>
       <input type="email" onChange={handleEmail}></input>
       <p>password</p>
       <input type="password" onChange={handlePassword}></input>
       <p>confirm password</p>
       <input type="password" onChange={handleConfirm}></input>
+      <p></p>
+      <button onClick={buttonClick}>Register</button>
+      </div>
     </div>
   );
 }
