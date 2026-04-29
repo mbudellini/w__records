325b3984bf7be344ef2266fc43e5daf9988ba3e0
mbudellini <matteo.budellini@gmail.com>
2026-04-29 10:50:16 +0200

    vercel deployment 2

--- src/api/config.js
+++ src/api/config.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API_BASE = window.location.href.includes('localhost')
   ? 'http://localhost:4444/api'
-  : 'https://wallpapers-by-alex.vercel.app/api';
+  : 'https://disqueriawhat.vercel.app/api';
 
 const api = axios.create({
   baseURL: API_BASE,
